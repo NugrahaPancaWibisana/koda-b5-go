9ecf7636f3df3c339d2312d772ef9e13e766010e
Nugraha Panca Wibisana <nugrahapancawibisana@gmail.com>
2025-12-23 14:41:05 +0700

    feat: adds minitask 2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println(HitungLingkaran(2))
+	SegitigaSikuSiku(10)
 }
 
 func HitungLingkaran(r int) string {
@@ -22,4 +23,36 @@ func LuasLingkaran(r int) int {
 	var x float64 = pi * float64(r*r)
 
 	return int(x)
+}
+
+func SegitigaSikuSiku(height int) {
+	// normal for
+	var starFor string = "*"
+
+	for i := 1; i < height; i++ {
+		fmt.Println(starFor)
+		starFor = starFor + "*"
+	}
+
+	fmt.Println("")
+
+	// while
+	var starWhile string = "*"
+
+	var j int = 1
+	for j < height {
+		fmt.Println(starWhile)
+		starWhile = starWhile + "*"
+		j++
+	}
+
+	fmt.Println("")
+
+	// range
+	var starRange string = "*"
+
+	for range height {
+		fmt.Println(starRange)
+		starRange = starRange + "*"
+	}
 }
